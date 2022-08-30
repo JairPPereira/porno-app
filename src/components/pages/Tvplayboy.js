e9ad3ba0009769a2be3370d7ff589e2439062860
JairPPereira <86321461+JairPPereira@users.noreply.github.com>
2022-08-30 15:26:46 -0300

    Update Tvplayboy.js

--- src/components/pages/Tvplayboy.js
+++ src/components/pages/Tvplayboy.js
@@ -1,58 +1,46 @@
-import React, { Component } from 'react';
-import './videoapp.css';
-import dashjs from 'dashjs';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import { Link } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+export default function Tvplayboy() {
+  return (
+    <div>
+          <p><br /></p>
+          <div><Container fluid>
+            <Row>
+              <Col>
+      <iframe
+        src="https://telefullenvivo.com/canales/playboy/"
+        target="_blank"
+        data-popup="false" 
+        frameborder="0"
+        width="100%" height="600px"
+        allow="autoplay; encrypted-media; fullscreen; pincture in pincture"
+        title="video"
+        data-draggable="false"
+      />{" "}
+      </Col></Row></Container>
+                  <div className="nave">
+     
+                  <p><br /></p>
 
-
- 
-class Tvplayboy extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-          url: null
-        };
-      }
-    
-      componentDidMount(){
-        let url = "https://cdn.cvattv.com.ar/live/c6eds/Playboy/SA_Live_dash_enc/Playboy.mpd";
-        let player = dashjs.MediaPlayer().create();
-        player.initialize(document.querySelector("#videoPlayer"), url, true);
-      }
-    
-      render() {
-        return (
-          <div>
-            <Container fluid>
-      <Row><p><br /></p>
-        <Col><video id="videoPlayer" crossorigin="anonymous" controls width="100%" height="auto"></video>
-        </Col>
-      </Row>
-    </Container><center>
-      
-    <nav>
-      <p><br /></p>
- 
-      <Link to="/"><Button variant="primary" size="lg" width="80%">
-      Home
-        </Button></Link>
+<Link to="/"><Button variant="primary" size="lg" width="80%">
+Home
+  </Button></Link>
 
 <p><br /></p>
+</div>
+</div></div>
 
-      </nav></center>
-          </div>
-        );
-      }
-    
-
+  );
 }
 
 
-export default Tvplayboy;
+
+
+
